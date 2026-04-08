5e12535b6ac75ca570e81dddd42f90b12f19faf8
bluenilegear <komoto@cliff-lab.com>
2026-04-08 19:56:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -136,13 +136,11 @@ app.get('/kawabou-dam/:date/:time/:twnCd', (req, res) => {
   const { date, time, twnCd } = req.params;
   fetchFromKawabou(`/kawabou/file/gjson/obs/${date}/${time}/dam/${twnCd}.json`, res);
 });
-
 // ダム時系列データ
 app.get('/kawabou-dam-tmlist/:date/:time/:fcd', (req, res) => {
   const { date, time, fcd } = req.params;
   fetchFromKawabou(`/kawabou/file/files/tmlist/dam/${date}/${time}/${fcd}.json`, res);
 });
-
 // ダムマスタ情報
 app.get('/kawabou-dam-master/:fcd', (req, res) => {
   const { fcd } = req.params;
